bd738f17eaa8001cb13d5dc7dd3f4483b53cb89e
Juan Carlos Pelaez <jucape7@gmail.com>
2019-05-30 22:47:36 -0500

    Interaccion con la galeria lista

--- public/scripts/galeriaMain.js
+++ public/scripts/galeriaMain.js
@@ -20,6 +20,30 @@ TweenMax.from(".filtros", 1, {
     delay:0.3 
 });
 
+
+
+$(".producto-galeria__imagenDos").mouseenter(function(){
+TweenMax.to(this, 0.5,{
+    opacity:1,
+    width: 250,
+    height: 250,
+    left: 0,
+    top: 0,
+    ease:Back.easeOut.config(1),
+});
+})
+
+$(".producto-galeria__imagenDos").mouseleave(function(){
+    TweenMax.to(this, 0.5,{
+        opacity:0,
+        width: 200,
+        height: 200,
+        left: '10%',
+        top: '10%',
+        ease:Back.easeOut.config(1),
+    });
+  })
+  
 }
 
 window.addEventListener('load', paginaCargada);
